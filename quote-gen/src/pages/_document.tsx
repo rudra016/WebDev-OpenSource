9dbec5da2102da7736cf0ce6245d0fb0c56fbe43
Dhruv Pankaj Patel <dhruvkochi@gmail.com>
2023-09-30 21:28:40 +0530

    feat: adding a simple frontend project

---
+++ quote-gen/src/pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body className='bg-yellow-400'>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
