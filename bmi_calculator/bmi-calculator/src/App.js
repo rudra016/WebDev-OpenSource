d9a1d229eea6a552027d4d462c4289ad132aa71d
VEDANSH <vedanshgupta09@gmail.com>
2023-10-03 19:23:33 +0530

    bmi calculator added

---
+++ bmi_calculator/bmi-calculator/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Calculator from './components/Calculator';
+function App() {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
