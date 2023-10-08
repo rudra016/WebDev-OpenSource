f73892c57df4495a6fc7799b3b91623b94a1af1c
Pankaj <pankajpj3103@gmail.com>
2023-10-08 16:03:54 +0530

    feat particle motion using tsparticle react

---
+++ ParticleMotionReact/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ParticlesBg from "./components/ParticleBg";
+import logo from "./logo.svg"
+
+export default function App() {
+ 
+  return (
+    <div className="App">
+      <ParticlesBg/>
+    </div>
+  );
+}
