1101a3e0ebc04976e44f45b46f5c7bddc70bad76
nirajan1111 <nirajansah1111@gmail.com>
2023-10-11 12:10:50 +0545

    Job_listing website frontend

---
+++ job_listing/src/components/Header/Roundedbox/roundedbox.js
@@ -0,0 +1,10 @@
+import './style.css'
+const Rounded_Rectangle = (props) => {
+
+    return (
+        <div className='roundedbox' style={{ top: props.top, left: props.left }}>
+
+        </div>
+    )
+}
+export default Rounded_Rectangle;
