5bcadc2bbbd520a51529fdfebe51f0b8719b7c3a
Harshit-Chordiya <harshitchordiya03@gmail.com>
2023-10-14 03:48:02 +0530

    added addrow and combinecolumn function  in script.js

--- 2048_Game/script.js
+++ 2048_Game/script.js
@@ -47,6 +47,97 @@ document.addEventListener('DOMContentLoaded', () =>  {
         }
       }
     }
+
+    
+    function right() {
+      for (let i=0; i < 16; i++) {
+        if (i % 4 === 0) {
+          let a = arr[i].innerHTML
+          let b = arr[i+1].innerHTML
+          let c = arr[i+2].innerHTML
+          let d = arr[i+3].innerHTML
+          let e = [parseInt(a), parseInt(b), parseInt(c), parseInt(d)]
+  
+          let change = e.filter(num => num)
+          let lost = 4 - change.length
+          let qq = Array(lost).fill(0)
+          let vara = qq.concat(change)
+  
+          arr[i].innerHTML = vara[0]
+          arr[i+3].innerHTML = vara[3]
+          arr[i+2].innerHTML = vara[2]
+          arr[i+1].innerHTML = vara[1]
+        }
+      }
+    }
+  
+   
+    function up() {
+      for (let i=0; i < 4; i++) {
+        let a = arr[i].innerHTML
+        let b = arr[i+w].innerHTML
+        let c = arr[i+(w*2)].innerHTML
+        let d = arr[i+(w*3)].innerHTML
+        let column = [parseInt(a), parseInt(b), parseInt(c), parseInt(d)]
+  
+        let filteredColumn = column.filter(num => num)
+        let lost = 4 - filteredColumn.length
+        let qq = Array(lost).fill(0)
+        let newColumn = filteredColumn.concat(qq)
+  
+        arr[i].innerHTML = newColumn[0]
+        arr[i +w].innerHTML = newColumn[1]
+        arr[i+(w*2)].innerHTML = newColumn[2]
+        arr[i+(w*3)].innerHTML = newColumn[3]
+      }
+    }
+  
+    function down() {
+      for (let i=0; i < 4; i++) {
+        let a = arr[i].innerHTML
+        let b = arr[i+w].innerHTML
+        let c = arr[i+(w*2)].innerHTML
+        let d = arr[i+(w*3)].innerHTML
+        let column = [parseInt(a), parseInt(b), parseInt(c), parseInt(d)]
+  
+        let filteredColumn = column.filter(num => num)
+        let lost = 4 - filteredColumn.length
+        let qq = Array(lost).fill(0)
+        let newColumn = qq.concat(filteredColumn)
+  
+        arr[i].innerHTML = newColumn[0]
+        arr[i +w].innerHTML = newColumn[1]
+        arr[i+(w*2)].innerHTML = newColumn[2]
+        arr[i+(w*3)].innerHTML = newColumn[3]
+      }
+    }
+
+
+    function addrow() {
+      for (let i =0; i < 15; i++) {
+        if (arr[i].innerHTML === arr[i +1].innerHTML) {
+          let sum = parseInt(arr[i].innerHTML) + parseInt(arr[i +1].innerHTML)
+          arr[i].innerHTML = sum
+          arr[i +1].innerHTML = 0
+          ans += sum
+          var_2.innerHTML = ans
+        }
+      }
+      check()
+    }
+  
+    function combineColumn() {
+      for (let i =0; i < 12; i++) {
+        if (arr[i].innerHTML === arr[i +w].innerHTML) {
+          let sum = parseInt(arr[i].innerHTML) + parseInt(arr[i +w].innerHTML)
+          arr[i].innerHTML = sum
+          arr[i +w].innerHTML = 0
+          ans += sum
+          var_2.innerHTML = ans
+        }
+      }
+      check()
+    }
   
   
   })
