235b60a4f9edcbe9ca83ea502b2ffa779b152e93
Him7n <knockknockletthedevilin777@proton.me>
2023-10-27 21:53:43 +0530

    added the animaation simulator with dark background and reflectin material and sparkles

---
+++ Animation Simulator/src/index.jsx
@@ -0,0 +1,24 @@
+import './style.css'
+import ReactDOM from 'react-dom/client'
+import { Canvas } from '@react-three/fiber'
+import Experience from './Experience.jsx'
+
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+
+root.render(
+    <Canvas
+        shadows
+        style={{ background: "black" }}
+        
+        color='black'
+        camera={ {
+            fov: 45,
+            near: 0.1,
+            far: 200,
+            position: [ - 4, 3, 6 ]
+        } }
+    >
+             {/* <color attach="background" args={[bgColor]} /> */}
+        <Experience />
+    </Canvas>
+)
